61c47fa3207c5afe282369d2db53452f27dffc53
al0nkr <alankardutta@gmail.com>
2023-07-29 01:44:37 +0530

    setup and requirements updated

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+    """
+    This function returns list of requirements 
+    """
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','' )for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+setup(
+    name = 'style-transfer',
+    version = '0.0.1',
+    author= 'alankar',
+    author_email='alankardutta@gmail.com',
+    packages = find_packages(),
+    install_requirements = get_requirements('requirements.txt')
+)
